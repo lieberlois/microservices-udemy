89b226ff8f5b6dadf4b8e7004d9496d7c7c22480
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-09-24 20:01:56 +0200

    Added basic UI for Blog

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
-  const commentId = randomBytes(4).toString("hex");
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
+
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
@@ -24,5 +27,5 @@ app.post("/posts/:id/comments", (req, res) => {
 });
 
 app.listen(4001, () => {
-  console.log("Listening on port 4001...");
+  console.log('Listening on 4001');
 });
