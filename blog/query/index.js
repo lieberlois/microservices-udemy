d7da5cec1ec5d306fc2d9fea06b724e830ec5309
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-09-24 22:06:50 +0200

    Adding moderation and Event persistence

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,27 +9,43 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type == "CommentCreated") {
-    const { id, content, postId } = data;
-    posts[postId].comments.push({ id, content });
+  if (type === "CommentCreated") {
+    const { id, content, status, postId } = data;
+    posts[postId].comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === "CommentUpdated") {
+    const { id, content, status, postId } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (_, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+  for (let event of res.data) {
+    console.log("Processing event: ", event.type);
+    handleEvent(event.type, event.data);
+  }
 });
