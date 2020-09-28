234f5b58e4d20755075f1e191ceb23a64c10148b
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-09-28 23:14:17 +0200

    Not Found Error and Async Error Handling

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +15,10 @@ app.use(currentUserRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 app.use(signinRouter);
+
+app.all("*", async (_req, _res, next) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
